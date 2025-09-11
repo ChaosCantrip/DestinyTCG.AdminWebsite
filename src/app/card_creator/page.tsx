7cc83fcbc52be6b7fc929356641a2719fbdae994
ChaosCantrip <dev@chaoscantrip.com>
2025-09-11 17:35:47 +0100

    Added card_creator Page

---
+++ src/app/card_creator/page.tsx
@@ -0,0 +1,7 @@
+export default function CardCreatorPage() {
+    return (
+        <div>
+            <h1>Card Creator Page</h1>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
+            <ul>
+                <li><Link href="/card-creator">Card Creator</Link></li>
+            </ul>
         </div>
     )
 }
